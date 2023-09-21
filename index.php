0a2bd0167b64527db0267031f5de70380396c653
macieledu <macieledu688@gmail.com>
2023-09-21 20:30:40 -0300

    primeiro commit

--- index.php
+++ index.php
@@ -20,8 +20,8 @@
     <link rel="stylesheet" href="css/stylesheet.css">
 </head>
 <body>
-    <h1>testando TCC</h1>
-    
+    <h1>testando</h1>
+    <i class="far fa-plus-square"></i>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.2/js/bootstrap.js" integrity="sha512-ipBeSMCnlAvS4AEbycy0nTk9KkYr5lUJwFHNvf4IxtV/CDW4qx53mZKUryWtNr6GFaBl11EXyrU6iE3mo6ib2g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
