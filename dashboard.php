a10a4d438b4b156a46a75ff2cc178139a4d9ac09
macieledu <macieledu688@gmail.com>
2023-11-09 20:45:26 -0300

    alterações no login, inicio de crud review

--- dashboard.php
+++ dashboard.php
@@ -2,15 +2,19 @@
  require_once("templates/header.php");
 
 
- // verifica se o user esta autenticadossss
- require_once("dao/userDAO.php");
+ // verifica se o user esta autenticado
+ require_once("dao/UserDAO.php");
+ require_once("dao/MovieDAO.php");
  require_once("models/User.php");
 
 
  $user = new User();
  $userDAO = new UserDao($conn, $BASE_URL);
+ $movieDAO = new MovieDAO($conn, $BASE_URL);
 
  $userData = $userDAO->verifyToken(true);
+
+ $userMovies = $movieDAO->getMoviesByUserId($userData->id);
 
 ?>
 
@@ -31,21 +35,25 @@
                 <th scope="col" class="actions-column">Ações</th>
               </thead>
               <tbody>
+                <?php foreach($userMovies as $movie): ?>
                 <tr>
-                    <td scope="row">1</td>
-                    <td><a href="#" class="table-movie-title">Título</a></td>
+                    <td scope="row"><?= $movie->id ?></td>
+                    <td><a href="<?= $BASE_URL ?>movie.php?id=<?= $movie->id ?>" class="table-movie-title"><?= $movie->title ?></a></td>
                     <td><i class="fas fa-star"></i> 9</td>
                     <td class="actions-column">
-                        <a href="" class="edit-btn">
+                        <a href="<?= $BASE_URL ?>editmovie.php?id=<?= $movie->id ?>" class="edit-btn">
                             <i class="far fa-edit"></i> Editar
                         </a>
-                        <form action="">
+                        <form action="<?= $BASE_URL ?>movie_process.php">
+                        <input type="hidden" name="type" value="delete">
+                        <input type="hidden" name="id" value="<?= $movie->id ?>">
                             <button type="submit" class="delete-btn">
                                 <i class="fas fa-times"></i> Deletar
                             </button>
                         </form>
                     </td>
                 </tr>
+                <?php endforeach; ?>
               </tbody>
             </table>
         </div>
--- user_process.php
+++ user_process.php
@@ -10,50 +10,45 @@
 
   $userDao = new UserDAO($conn, $BASE_URL);
 
-  // Resgata o tipo do formulário
+  // resgata o tipo do formulário
   $type = filter_input(INPUT_POST, "type");
 
-  // Atualizar usuário
   if($type === "update") {
 
-    // Resgata dados do usuário
     $userData = $userDao->verifyToken();
 
-    // Receber dados do post
+    // recebe dados
     $name = filter_input(INPUT_POST, "name");
     $lastname = filter_input(INPUT_POST, "lastname");
     $email = filter_input(INPUT_POST, "email");
     $bio = filter_input(INPUT_POST, "bio");
 
-    // Criar um novo objeto de usuário
     $user = new User();
 
-    // Preencher os dados do usuário
     $userData->name = $name;
     $userData->lastname = $lastname;
     $userData->email = $email;
     $userData->bio = $bio;
 
-  // Upload da imagem
+  // upload da imagem
   if(isset($_FILES["image"]) && !empty($_FILES["image"]["tmp_name"])) {
     
     $image = $_FILES["image"];
     $imageTypes = ["image/jpeg", "image/jpg", "image/png"];
     $jpgArray = ["image/jpeg", "image/jpg"];
 
-    // Checagem de tipo de imagem
     if(in_array($image["type"], $imageTypes)) {
 
-      // Checar se jpg ou png
+    // checar se jpg ou png
     if(in_array($image["type"], $jpgArray)) {
       $imageFile = imagecreatefromjpeg($image["tmp_name"]);
     } else {
       $imageFile = imagecreatefrompng($image["tmp_name"]);
 
-      // Verifica se a criação da imagem PNG foi bem-sucedida
+      // verifica se a criação da imagem PNG foi bem-sucedida
       if(!$imageFile) {
           $message->setMessage("Falha ao processar a imagem PNG. Certifique-se de que é uma imagem válida.", "error", "back");
-          exit; // Ou redirecione para onde for apropriado em seu caso
+          exit; // ou redireciona
       }
     }
 
